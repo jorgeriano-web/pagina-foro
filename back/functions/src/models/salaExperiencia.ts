2b7696a6ad3598499bb5193277b3d67b4e76d936
jorge.riano@segurosbolivar.com-Desarrollador <jorge.riano@segurosbolivar.com>
2026-03-26 17:05:21 -0500

    ajustes para reserva cupos

---
+++ back/functions/src/models/salaExperiencia.ts
@@ -0,0 +1,7 @@
+/** Documento en Firestore: salas_experiencias / {id} */
+export interface SalaExperiencia {
+  id: string;
+  nombre: string;
+  capacidadTotal: number;
+  cuposReservados: number;
+}
